02ed1268d3ce810bc46eff58712493a0fa97a7f2
Vaibhav Sharma <vaibhavsharma987s6@gmail.com>
2021-09-12 20:08:03 +0530

    update and delete database functionalities completed

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { editExpense, removeExpense } from "../actions/expenses";
+import { startEditExpense, startRemoveExpense } from "../actions/expenses";
 import ExpenseForm from "./ExpenseForm";
 
 const EditExpensePage = (props) => {
@@ -13,7 +13,7 @@ const EditExpensePage = (props) => {
       <ExpenseForm
         expense={props.expense}
         onSubmit={(expense) => {
-          props.dispatch(editExpense(props.expense.id, expense));
+          props.dispatch(startEditExpense(props.expense.id, expense));
           props.history.push("/");
         }}
       />
@@ -26,7 +26,7 @@ const EditExpensePage = (props) => {
       </button>
       <button
         onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id }));
+          props.dispatch(startRemoveExpense({ id: props.expense.id }));
           history.push("/");
         }}
       >
